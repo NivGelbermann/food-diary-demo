421a02fae75db0c025e8bd2a2b6c3fe52ead1cb6
Niv Gelbermann <nivgmann@gmail.com>
2017-09-26 04:55:10 +0300

    Category/Date/Time rows #2
    Decided to stick with the RecyclerView approach to displaying the rows.
    Updated the AddEditRecyclerViewAdapter. 
    All that's left (until implementing categories) is to add displaying dialogs for choosing date & time, which can wait until add_edit_layout is fully working in runtime.

--- app/src/main/java/com/nivgelbermann/fooddiarydemo/AddEditRecyclerViewAdapter.java
+++ app/src/main/java/com/nivgelbermann/fooddiarydemo/AddEditRecyclerViewAdapter.java
@@ -14,12 +14,16 @@ import butterknife.ButterKnife;
  * Created by Niv on 25-Sep-17.
  */
 
-class AddEditRecyclerViewAdapter extends RecyclerView.Adapter<AddEditRecyclerViewAdapter.ItemContentViewHolder> {
+class AddEditRecyclerViewAdapter extends RecyclerView.Adapter<AddEditRecyclerViewAdapter.ItemContentViewHolder>
+                                 implements RecyclerItemClickListener.OnRecyclerClickListener{
     private static final String TAG = "AddEditRecyclerViewAdap";
 
     private Context mContext;
 
     private FoodItem mItem;
+    private static final int POS_CATEGORY = 0;
+    private static final int POS_DATE = 1;
+    private static final int POS_TIME = 2;
 
     AddEditRecyclerViewAdapter(Context context, FoodItem foodItem) {
         mContext = context;
@@ -42,17 +46,70 @@ class AddEditRecyclerViewAdapter extends RecyclerView.Adapter<AddEditRecyclerVie
 
     @Override
     public ItemContentViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        Log.d(TAG, "onCreateViewHolder: new view requested);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.add_edit_row_details, parent, false);
+        return new ItemContentViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ItemContentViewHolder holder, int position) {
-
+        Log.d(TAG, "onBindViewHolder: starts");
+        
+        switch (position) {
+            case POS_CATEGORY:
+                holder.icon.setImageResource(R.drawable.ic_cake_grey_700_24dp);
+                holder.header.setText("Category"); // TODO Create string resource
+                holder.content.setText("FOOD"); // Temporary - until implementing categories
+                break;
+                
+            case POS_DATE:
+                holder.icon.setImageResource(R.drawable.ic_event_grey_700_24dp);
+                holder.header.setText("Date"); // TODO Create string resource
+                holder.content.setText("FOOD"); // Temporary - until implementing categories
+                break;
+                
+            case POS_TIME:
+                holder.icon.setImageResource(R.drawable.ic_access_time_grey_700_24dp);
+                holder.header.setText("Date"); // TODO Create string resource
+                holder.content.setText("FOOD"); // Temporary - until implementing categories
+                break;
+                
+            default:
+                throw new InvalidParameterException(TAG + ".onBindViewHolder called with invalid position: " + position);
+        }
+        
+        Log.d(TAG, "onBindViewHolder: ends");
     }
 
     @Override
     public int getItemCount() {
         // There are always 3 items: Category, Date, Hour
         return 3;
+    }
+    
+    @Override
+    public void onItemClick(View view, int position) {
+        Log.d(TAG, "onItemClick: called");
+        
+        // Temporary click verification
+        Toast.makeText(mContext, "Row " + position + " clicked", Toast.LENGTH_SHORT).show();
+        
+        switch (position) {
+            // TODO Handle each case 
+            case POS_CATEGORY:
+                // select category
+                break;
+                
+            case POS_DATE:
+                // display dialog for choosing date
+                break;
+                
+            case POS_TIME:
+                // display dialog for choosing time
+                break;
+                
+            default:
+                throw new InvalidParameterException(TAG + " onItemClick called with invalid position " + position);
+        }
     }
 }
